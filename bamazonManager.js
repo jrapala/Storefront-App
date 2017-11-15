757b46c4bd0d29de53d7693b0c6ef5e601f831d0
Juliette Rapala <jrapala@gmail.com>
2017-11-15 16:29:10 -0600

    Adding increase inventory function.

--- bamazonManager.js
+++ bamazonManager.js
@@ -54,8 +54,7 @@
             viewLowInventory();
             break;
           case ('Add to Inventory'):
-            console.log('Option 3');
-            //addToInventory();
+            addToInventory();
             break;
           case ('Add New Product'):
             console.log('Option 4');
@@ -81,6 +80,7 @@
         if (err) throw err;
         // Log all results of the SELECT statement
         console.table(res);
+        // Return to prompts
         promptUser();
       });
     }
@@ -96,11 +96,115 @@
           promptUser();
         // Display any products that have less than 5 items in stock.
         } else {
+          console.log(" ");
           console.table(res);
+          // Return to prompts
           promptUser();
         }
       });
     }
+
+    // Update product
+    function updateProduct(productId, newQuantity) {
+      connection.query("UPDATE products SET ? WHERE ?",
+      [
+        {
+          stock_quantity: newQuantity
+        },
+        {
+          item_id: productId
+        }
+      ],
+      function(err, res) {
+        // Error handling
+        if (err) throw err;
+      });
+    };
+
+
+    function addToInventory() {
+      inquirer.prompt([{
+        // Prompt for ID number
+        name: "idOfProduct",
+        type: "input",
+        message: "What is the ID number of the product you wish to increase the inventory of? (Press Q to Exit)",
+        // If Q, quit. If any other letter, ask for numerical input.
+        validate: function(userInput) {
+          if (userInput.toUpperCase() === "Q") {
+            console.log("\n\n");
+            promptUser();
+          } else if (isNumber(userInput)) {
+            return true;
+          } else {
+            console.log("\n\nYou did not choose a numerical ID. Please try again!\n");
+            return false;
+          }
+        }},{
+        // Prompt for quantity 
+        name: "quantityOfProduct",
+        type: "input",
+        message: "How many units would you add? (Press Q to Exit)",
+        // If Q, quit. If any other letter, ask for numerical input.
+        validate: function(userInput) {
+          if (userInput.toUpperCase() === "Q") {
+            console.log("\n\n");
+            promptUser();
+          } else if (isNumber(userInput)) {
+            return true;
+          } else {
+            console.log("\nYou did not choose a numerical quantity. Please try again!\n");
+            return false;
+          }
+      }}]).then(function(response) {  
+        // Variables
+        var productId = response.idOfProduct;
+        var productName;
+        var quantityToAdd = parseInt(response.quantityOfProduct);
+        var currentQuantity;
+        var newQuantity;
+        connection.query("SELECT * FROM products WHERE item_id = ?", [productId], function(err, res) {
+          // Error handling
+          if (err) throw err;
+          // Find current quantity of product
+          for (var i = 0; i < res.length; i++) {
+            productName = res[i].product_name;
+            currentQuantity = parseInt(res[i].stock_quantity);
+          };
+          // Handler if product is not in the database
+          if (productName === undefined) {
+            console.log(`\nSorry, we do not have a product with that ID number.\n`);
+            promptUser();
+          } else {
+            // Find new quantity of product
+            newQuantity = currentQuantity + quantityToAdd;
+            // Update product in database
+            updateProduct(productId, newQuantity);
+            console.log(`\n${quantityToAdd} units of ${productName} added. The new quantity in stock is ${newQuantity}.\n`)
+            // Return to prompts
+            promptUser();
+          }
+        });
+      });
+    };
+
+
+    //     // Get current inventory
+    //     connection.query("UPDATE products SET ? WHERE ?",
+    //   [
+    //     {
+    //       stock_quantity: newQuantity
+    //     },
+    //     {
+    //       item_id: productId
+    //     }
+    //   ],
+    //   function(err, res) {
+    //     // Error handling
+    //     if (err) throw err;
+    //   });
+
+    //     var productId = response
+    // }
 
     // // Update product
     // function updateProduct(productId, newQuantity) {
